747f8d14de49f01442be844ec2675b6be018f990
SarahSchmid <sarah.schmid@codecentric.de>
2018-06-12 12:07:28 +0200

    Added navigation

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {NavigationBar} from "./components/NavigationBar";
 import {Startpage} from './components/startpage/Startpage';
 import './App.css';
 
@@ -6,6 +7,7 @@ class App extends Component {
   render() {
     return (
         <div className="App">
+            <NavigationBar/>
             <Startpage/>
         </div>
     );
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import "../styles/NavigationBar.css";
+
+export class NavigationBar extends Component{
+
+    render(){
+        return (
+            <div>
+                <ul className="NavigationBar">
+                    <li className="navigation-item name"><a className="link" href="#Startpage">Sarah Schmid</a></li>
+                    <li className="navigation-item last"><a className="link" href="#Contact">Contact</a></li>
+                    <li className="navigation-item"><a className="link" href="#Resume">Resume</a></li>
+                    <li className="navigation-item"><a className="link" href="#Projects">Projects</a></li>
+                    <li className="navigation-item"><a className="link" href="#About">About</a></li>
+                </ul>
+            </div>
+        );
+    }
+}
