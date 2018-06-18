8df6e7509bd8d0d3804d7a23d9f60a2767519283
SarahSchmid <sarah.schmid@codecentric.de>
2018-06-18 22:23:12 +0200

    changed navigation order

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -8,8 +8,8 @@ export class NavigationBar extends Component{
             <ul className="NavigationBar">
                 <li className="navigation-item name"><a className="link" href="#Startpage">Sarah Schmid</a></li>
                 <li className="navigation-item last"><a className="link" href="#Contact">Contact</a></li>
-                <li className="navigation-item"><a className="link" href="#Resume">Resume</a></li>
                 <li className="navigation-item"><a className="link" href="#Projects">Projects</a></li>
+                <li className="navigation-item"><a className="link" href="#Timeline">Timeline</a></li>
                 <li className="navigation-item"><a className="link" href="#About">About</a></li>
             </ul>
 
