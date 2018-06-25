a59034cffa2f8a464eefb3e958e148c2d6843604
SarahSchmid <sarah.schmid@codecentric.de>
2018-06-25 11:26:31 +0200

    moven CV dates to export object

--- src/components/timeline/Timeline.js
+++ src/components/timeline/Timeline.js
@@ -8,20 +8,6 @@ import '../../App.css';
 import {TimelineItem} from "./TimelineItem";
 import CVData from "./content";
 
-const dates = [ '07/01/2007',
-                '09/01/2007',
-                '08/01/2008',
-                '09/01/2011',
-                '01/01/2012',
-                '10/01/2013',
-                '02/01/2014',
-                '08/01/2015',
-                '04/13/2016',
-                '07/12/2016',
-                '06/01/2017',
-                '09/01/2017',
-                '04/15/2018'];
-
 export class Timeline extends Component{
     constructor(props){
      super(props);
@@ -40,7 +26,9 @@ export class Timeline extends Component{
                                             indexClick={(index) => {
                                                 this.setState({ value: index, previous: this.state.value });
                                             }}
-                                            values={ dates }
+                                            values={
+                                                CVData.map(dates => dates.date)
+                                            }
                                             styles={{
                                                 background: '#f8f8f8',
                                                 foreground: '#129198',
--- src/components/timeline/TimelineItem.js
+++ src/components/timeline/TimelineItem.js
@@ -10,7 +10,7 @@ export class TimelineItem extends Component {
             <div key={this.props.data}>
                 <h2 className="timelineTitle">{this.props.data.title}</h2>
                 <i className="timelineSubtitle">{this.props.data.subtitle}</i>
-                <p className="timelineDate">{this.props.data.date}</p>
+                <p className="timelineDate">{this.props.data.fullDate}</p>
                 <div className="foobar">
                     <p className="timelineDescription">{this.props.data.description}</p>
                 </div>
