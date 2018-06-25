4bf83eb2129c1771ac0b248f1bd0249eb8d03e9e
SarahSchmid <sarah.schmid@codecentric.de>
2018-06-25 10:58:16 +0200

    undo last commit

--- src/components/projects/Project.js
+++ src/components/projects/Project.js
@@ -1,25 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import '../../styles/Projects.css';
 import '../../App.css';
 
 const target = "_blank";
 
-const Project =() => {
-    return (
-        <div className="item">
-            <img src={this.props.imgSrc} alt="Project" className="image"/>
-            <div className="overlay">
-                <div className="projectDescription">
-                    <h1>{this.props.title}</h1>
-                    <p>{this.props.description}</p>
-                    <p><a target={target} href={this.props.repoUrl}>Click here for the repository</a></p>
-                    <p><a target={target} href={this.props.appUrl}>Click here for the application</a></p>
+export class Project extends Component{
+    render(){
+        return (
+            <div className="item">
+                <img src={this.props.imgSrc} alt="Project" className="image"/>
+                <div className="overlay">
+                    <div className="projectDescription">
+                        <h1>{this.props.title}</h1>
+                        <p>{this.props.description}</p>
+                        <p><a target={target} href={this.props.repoUrl}>Click here for the repository</a></p>
+                        <p><a target={target} href={this.props.appUrl}>Click here for the application</a></p>
+                    </div>
                 </div>
+
             </div>
 
-        </div>
-
-    );
-};
-
-export default Project;
+        );
+    }
+}
