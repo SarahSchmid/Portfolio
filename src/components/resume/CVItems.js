70411beb3c2b83632b4867aa88ef4cec033b4230
SarahSchmid <sarah.schmid@codecentric.de>
2018-06-27 10:45:31 +0200

    removed dummy descriptions

--- src/components/resume/CVItems.js
+++ src/components/resume/CVItems.js
@@ -4,91 +4,91 @@ module.exports= [
         fullDate: "July 2007",
         title: "General Certificate of Secondary Education",
         subtitle: "Privates Kurpfalz-Internat, Bammental, Germany",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget quam non urna rutrum consectetur vitae quis lacus. Nam suscipit dignissim dui, id cursus est auctor et. Vestibulum arcu neque, mattis nec tincidunt vitae, auctor eu nunc. Cras sapien purus, ultricies vitae velit eget, laoreet iaculis erat. Nunc volutpat luctus justo vitae dictum. Donec commodo congue sem, et volutpat lectus sollicitudin vitae. Phasellus venenatis lobortis dui id consequat. Sed tincidunt nisi sed odio aliquet, vel egestas magna faucibus. Proin egestas nulla eget lacinia ultrices. Duis risus neque, efficitur sed viverra et, sagittis vel felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus."
+        description: ""
     },
     {
         date: '09/01/2007',
         fullDate: "01. September 2007 - 15. April 2008",
         title: "Apprenticeship as Hotel Specialist",
         subtitle: "Best Western Hotel Der Föhrenhof, Hanover, Germany",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget quam non urna rutrum consectetur vitae quis lacus. Nam suscipit dignissim dui, id cursus est auctor et. Vestibulum arcu neque, mattis nec tincidunt vitae, auctor eu nunc. Cras sapien purus, ultricies vitae velit eget, laoreet iaculis erat. Nunc volutpat luctus justo vitae dictum. Donec commodo congue sem, et volutpat lectus sollicitudin vitae. Phasellus venenatis lobortis dui id consequat. Sed tincidunt nisi sed odio aliquet, vel egestas magna faucibus. Proin egestas nulla eget lacinia ultrices. Duis risus neque, efficitur sed viverra et, sagittis vel felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. "
+        description: ""
     },
     {
         date: '08/01/2008',
         fullDate: "01. August 2008 - 01. December 2011",
         title: "Waitress",
         subtitle: "Wiking Hotel, Henstedt-Ulzburg, Germany",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget quam non urna rutrum consectetur vitae quis lacus. Nam suscipit dignissim dui, id cursus est auctor et. Vestibulum arcu neque, mattis nec tincidunt vitae, auctor eu nunc. Cras sapien purus, ultricies vitae velit eget, laoreet iaculis erat. Nunc volutpat luctus justo vitae dictum. Donec commodo congue sem, et volutpat lectus sollicitudin vitae. Phasellus venenatis lobortis dui id consequat. Sed tincidunt nisi sed odio aliquet, vel egestas magna faucibus. Proin egestas nulla eget lacinia ultrices. Duis risus neque, efficitur sed viverra et, sagittis vel felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. "
+        description: ""
     },
     {
         date: '09/01/2011',
         fullDate: "01. September 2011 - 15. May 2013",
         title: "Degree Program: Games Development",
         subtitle: "SAE Institute, Hamburg, Germany",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget quam non urna rutrum consectetur vitae quis lacus. Nam suscipit dignissim dui, id cursus est auctor et. Vestibulum arcu neque, mattis nec tincidunt vitae, auctor eu nunc. Cras sapien purus, ultricies vitae velit eget, laoreet iaculis erat. Nunc volutpat luctus justo vitae dictum. Donec commodo congue sem, et volutpat lectus sollicitudin vitae. Phasellus venenatis lobortis dui id consequat. Sed tincidunt nisi sed odio aliquet, vel egestas magna faucibus. Proin egestas nulla eget lacinia ultrices. Duis risus neque, efficitur sed viverra et, sagittis vel felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. "
+        description: ""
     },
     {
         date: '01/01/2012',
         fullDate: "01. January 2012 - 31. March 2016",
         title: "Waitress",
         subtitle: "Best Western Hotel Schmöker-Hof, Norderstedt, Germany",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget quam non urna rutrum consectetur vitae quis lacus. Nam suscipit dignissim dui, id cursus est auctor et. Vestibulum arcu neque, mattis nec tincidunt vitae, auctor eu nunc. Cras sapien purus, ultricies vitae velit eget, laoreet iaculis erat. Nunc volutpat luctus justo vitae dictum. Donec commodo congue sem, et volutpat lectus sollicitudin vitae. Phasellus venenatis lobortis dui id consequat. Sed tincidunt nisi sed odio aliquet, vel egestas magna faucibus. Proin egestas nulla eget lacinia ultrices. Duis risus neque, efficitur sed viverra et, sagittis vel felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. "
+        description: ""
     },
     {
         date: '10/01/2013',
         fullDate: "01. October 2013 - 31. December 2013",
         title: "Internship Software Development",
         subtitle:"Dating Cafe Vermittlungsagentur GmbH, Hamburg, Germany",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget quam non urna rutrum consectetur vitae quis lacus. Nam suscipit dignissim dui, id cursus est auctor et. Vestibulum arcu neque, mattis nec tincidunt vitae, auctor eu nunc. Cras sapien purus, ultricies vitae velit eget, laoreet iaculis erat. Nunc volutpat luctus justo vitae dictum. Donec commodo congue sem, et volutpat lectus sollicitudin vitae. Phasellus venenatis lobortis dui id consequat. Sed tincidunt nisi sed odio aliquet, vel egestas magna faucibus. Proin egestas nulla eget lacinia ultrices. Duis risus neque, efficitur sed viverra et, sagittis vel felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. "
+        description: ""
     },
     {
         date: '02/01/2014',
         fullDate: "01. February 2014 - 11. July 2016",
         title: "Apprenticeship as IT-Specialist for Software Development",
         subtitle:"Dating Cafe Vermittlungsagentur GmbH (later Dating Cafe Online GmbH), Hamburg, Germany",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget quam non urna rutrum consectetur vitae quis lacus. Nam suscipit dignissim dui, id cursus est auctor et. Vestibulum arcu neque, mattis nec tincidunt vitae, auctor eu nunc. Cras sapien purus, ultricies vitae velit eget, laoreet iaculis erat. Nunc volutpat luctus justo vitae dictum. Donec commodo congue sem, et volutpat lectus sollicitudin vitae. Phasellus venenatis lobortis dui id consequat. Sed tincidunt nisi sed odio aliquet, vel egestas magna faucibus. Proin egestas nulla eget lacinia ultrices. Duis risus neque, efficitur sed viverra et, sagittis vel felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. "
+        description: ""
     },
     {
-        date: '08/01/2015',
+        date: '07/01/2015',
         fullDate: "01. August 2015 - 30. November 2016",
         title: "Scrum Master",
         subtitle:"Dating Cafe Online GmbH, Hamburg, Germany",
-        description: "During my apprenticeship I became the Scrum Master of our small team. "
+        description: ""
     },
     {
         date: '04/13/2016',
         fullDate: "13. April 2016",
         title: "Qualification as Certified Scrum Master (CSM)",
         subtitle: "Scrum Alliance, Inc. / it-agile GmbH, Hamburg, Germany",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget quam non urna rutrum consectetur vitae quis lacus. Nam suscipit dignissim dui, id cursus est auctor et. Vestibulum arcu neque, mattis nec tincidunt vitae, auctor eu nunc. Cras sapien purus, ultricies vitae velit eget, laoreet iaculis erat. Nunc volutpat luctus justo vitae dictum. Donec commodo congue sem, et volutpat lectus sollicitudin vitae. Phasellus venenatis lobortis dui id consequat. Sed tincidunt nisi sed odio aliquet, vel egestas magna faucibus. Proin egestas nulla eget lacinia ultrices. Duis risus neque, efficitur sed viverra et, sagittis vel felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. "
+        description: ""
     },
     {
         date: '07/12/2016',
         fullDate: "12. July 2016 - 30. November 2016",
         title: "Software Developer",
         subtitle: "Dating Cafe Online GmbH, Hamburg Germany",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget quam non urna rutrum consectetur vitae quis lacus. Nam suscipit dignissim dui, id cursus est auctor et. Vestibulum arcu neque, mattis nec tincidunt vitae, auctor eu nunc. Cras sapien purus, ultricies vitae velit eget, laoreet iaculis erat. Nunc volutpat luctus justo vitae dictum. Donec commodo congue sem, et volutpat lectus sollicitudin vitae. Phasellus venenatis lobortis dui id consequat. Sed tincidunt nisi sed odio aliquet, vel egestas magna faucibus. Proin egestas nulla eget lacinia ultrices. Duis risus neque, efficitur sed viverra et, sagittis vel felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. "
+        description: ""
     },
     {
         date: '06/01/2017',
         fullDate: "01. June 2017 - 31. July 2017",
         title: "Internship Android development",
         subtitle: "Mobile Trend GmbH, Hamburg, Germany",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget quam non urna rutrum consectetur vitae quis lacus. Nam suscipit dignissim dui, id cursus est auctor et. Vestibulum arcu neque, mattis nec tincidunt vitae, auctor eu nunc. Cras sapien purus, ultricies vitae velit eget, laoreet iaculis erat. Nunc volutpat luctus justo vitae dictum. Donec commodo congue sem, et volutpat lectus sollicitudin vitae. Phasellus venenatis lobortis dui id consequat. Sed tincidunt nisi sed odio aliquet, vel egestas magna faucibus. Proin egestas nulla eget lacinia ultrices. Duis risus neque, efficitur sed viverra et, sagittis vel felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. "
+        description: ""
     },
     {
         date: '09/01/2017',
         fullDate: "01. September 2017 - 21. December 2017",
         title: "IT-Consultant",
         subtitle: "ALPHA COM Deutschland GmbH, Hamburg, Germany",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget quam non urna rutrum consectetur vitae quis lacus. Nam suscipit dignissim dui, id cursus est auctor et. Vestibulum arcu neque, mattis nec tincidunt vitae, auctor eu nunc. Cras sapien purus, ultricies vitae velit eget, laoreet iaculis erat. Nunc volutpat luctus justo vitae dictum. Donec commodo congue sem, et volutpat lectus sollicitudin vitae. Phasellus venenatis lobortis dui id consequat. Sed tincidunt nisi sed odio aliquet, vel egestas magna faucibus. Proin egestas nulla eget lacinia ultrices. Duis risus neque, efficitur sed viverra et, sagittis vel felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. "
+        description: ""
     },
     {
         date: '04/15/2018',
         fullDate: "15. April 2018 - 13. July 2018",
         title: "Internship Agile Software Factory & Consulting",
         subtitle: "codecentric AG, Solingen/Hamburg, Germany",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget quam non urna rutrum consectetur vitae quis lacus. Nam suscipit dignissim dui, id cursus est auctor et. Vestibulum arcu neque, mattis nec tincidunt vitae, auctor eu nunc. Cras sapien purus, ultricies vitae velit eget, laoreet iaculis erat. Nunc volutpat luctus justo vitae dictum. Donec commodo congue sem, et volutpat lectus sollicitudin vitae. Phasellus venenatis lobortis dui id consequat. Sed tincidunt nisi sed odio aliquet, vel egestas magna faucibus. Proin egestas nulla eget lacinia ultrices. Duis risus neque, efficitur sed viverra et, sagittis vel felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. "
+        description: ""
     }
 
 ];
