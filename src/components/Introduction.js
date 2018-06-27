17027836e3819b6314d7ccfc1c868d1d64b0f77a
SarahSchmid <sarah.schmid@codecentric.de>
2018-06-27 15:32:26 +0200

    replaced dummy text with real introduction

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -16,11 +16,25 @@ const Introduction = () => {
                     <img src={introductionPicture} alt="Introduction"/>
                 </div>
                 <div className="description">
-                    <strong>Lorem ipsum dolor sit amet</strong>, consectetur adipiscing elit. Maecenas porta congue lobortis. Nullam facilisis blandit egestas. Quisque at ultrices dolor. Sed interdum turpis euismod vulputate venenatis. Phasellus congue porta libero, et suscipit mauris tincidunt at. In sit amet mauris lacinia, fermentum tortor vitae, euismod quam. Mauris porta mollis ultricies. Etiam scelerisque massa ultricies metus sodales efficitur. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In et elit lacinia, convallis tortor ut, bibendum risus. Proin mauris lorem, faucibus eleifend diam non, varius facilisis augue.
+                    <p>I'm a full stack engineer, with a passion for front end. I'm fluent in Java,
+                        Groovy and JavaScript and quite competent in frameworks and technologies like Grails, Spring,
+                        React, Docker and Git. I love getting the opportunity to learn an create new things almost every
+                        day and I'm sure that I'll do it for the rest of my life.</p>
 
-                    Ut sed placerat lorem, semper lacinia leo. Praesent aliquam velit et scelerisque varius. Praesent varius gravida turpis ut fringilla. Pellentesque ut ligula felis. Morbi velit enim, consequat at gravida et, ornare eu erat. Quisque id euismod odio. Sed et mauris ac velit pharetra ultricies posuere nec est. Proin sit amet diam vitae sem venenatis fermentum ultrices sed lorem. Cras faucibus, magna sed luctus iaculis, nisi lorem fringilla sapien, vulputate feugiat turpis sapien id dolor. Donec venenatis velit nec erat commodo, ac facilisis magna condimentum. In eros nibh, mollis ut dignissim nec, dictum eget metus. Suspendisse potenti. Aliquam quis congue eros, fringilla porta lacus.
+                    <p>I was born and raised in Hamburg. My parents were both hotel managers.
+                        In fact it was pretty clear that I had to try it. So I started an apprenticeship as a hotel
+                        specialist, but I realized soon that it's not the kind of job I want to do. I wanted something
+                        creative . I stumbled through some jobs over a few years, not knowing what I want to do but a
+                        lucky coincidence happened as I ran into the booth of the SAE Institute on a convention. Two
+                        months later I studied Games Programming at the SAE Institute, followed by an apprenticeship as
+                        IT-Specialist for Software Development. I immediately knew that coding was my passion.
+                    </p>
+                    <p>I'm also a passionate traveler, paintball player and singer. So if you don't mind that I'll start
+                        singing every once in a while, I could be a good fit for your team ;)
+                    </p>
+                    <a href="mailto:sarah@schmid.hamburg?Subject=Hello%20there!" target="_top">Let me know if you're interested!</a>
 
-                    Morbi sit amet mauris sit amet ex suscipit venenatis. Nulla pulvinar sagittis ante, vel fringilla dui vehicula nec. Nullam ultrices ipsum urna, vel lobortis lacus condimentum vel. Mauris quis vestibulum urna. Praesent facilisis commodo laoreet. Integer congue turpis eu iaculis ullamcorper. Donec turpis odio, consectetur quis odio ut, accumsan tincidunt turpis. Sed nec placerat turpis. Donec a luctus libero, a cursus neque. Donec suscipit sit amet massa id volutpat. Etiam molestie neque rhoncus tellus blandit vehicula. Mauris augue quam, porttitor nec orci quis, fermentum vestibulum leo. In hac habitasse platea dictumst. Vivamus aliquet non libero sit amet porttitor. Curabitur sit amet nisi faucibus, tempus tortor a, molestie risus. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+
                 </div>
             </div>
         </div>
