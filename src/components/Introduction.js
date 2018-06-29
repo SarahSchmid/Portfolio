ee94ca69558cb6abb45242228434a6ed30f3a682
SarahSchmid <sarah.schmid@codecentric.de>
2018-06-29 16:32:23 +0200

    updated introduction

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -22,10 +22,16 @@ const Introduction = () => {
                         creative. I stumbled through some jobs over a few years, not knowing what I want to do but a
                         lucky coincidence happened as I ran into the booth of the SAE Institute on a convention. Two
                         months later I studied Games Programming at the SAE Institute, followed by an apprenticeship as
-                        IT-Specialist for Software Development. I immediately knew that coding was my passion.
+                        IT-Specialist for Software Development. During my apprenticeship, I also became Scrum Mistress of our
+                        small Team. My retrospectives were legendary and furthermore, I took care that my team could work
+                        properly. Scrum Mastery is really fun, but I also knew that coding was my passion.
+                        I graduated my apprenticeship a half year earlier than normal and became a real software engineer. Yaaay!
+                    </p>
+                    <p>
+                        Since then, I'm trying to get experience and improving myself every day.
                     </p>
                     <p>I'm also a passionate traveler, paintball player and singer. So if you don't mind that I'll start
-                        singing every once in a while, I could be a good fit for your team ;)
+                        singing every once in a while, I could be a great fit for your team ;)
                     </p>
                     <a href="mailto:sarah@schmid.hamburg?Subject=Hello%20there!" target="_top">Let me know if you're interested!</a>
 
