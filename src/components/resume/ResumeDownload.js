9ac77e68babcd3b298571956222f3ad8afcc2352
SarahSchmid <sarah.schmid@codecentric.de>
2018-06-29 16:02:30 +0200

    updated download link

--- src/components/resume/ResumeDownload.js
+++ src/components/resume/ResumeDownload.js
@@ -5,7 +5,7 @@ import {faFilePdf} from '@fortawesome/fontawesome-free-regular';
 const ResumeDownload = () => {
     return(
         <div className="download">
-            <a className="downloadButton" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/open?id=1XyPq2grmA-zCOjTFLO50XPA-nCUJa32-">
+            <a className="downloadButton" target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1vbCrY0ycdLa9nir9ihXz05cB8g6oQgnQ/view?usp=sharing">
                 <FontAwesomeIcon icon={faFilePdf}/> Resume</a>
         </div>
     );
